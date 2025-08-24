b3f10bfa2052da017b75fa7685f54eaf80b9b2f0
SW33TSTUFF <simaakniyaz@gmail.com>
2025-08-24 13:40:23 +0530

    Initialize Spring Boot project with Maven wrapper and configuration files. Added .gitignore, .gitattributes, and pom.xml for project setup. Included application properties for database connection and basic test class.

---
+++ src/test/java/com/teacollection/teacollection_backend/TeacollectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacollection.teacollection_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacollectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
